f4e2eca03c15638096759f0f6214ed9bd41db7b1
Matteo Meneguzzi <matteo.meneguzzi@temera.it>
2022-07-21 16:29:34 +0200

    7.6: anecdotes and hooks step3

--- src/components/CreateNew.js
+++ src/components/CreateNew.js
@@ -1,23 +1,30 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useField } from '../hooks/index';
 
 const CreateNew = ({ addNew, setNotification }) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('text');
+  const contentCopy = { ...content };
+  content.reset = null;
+  const author = useField('text');
+  const authorCopy = { ...author };
+  author.reset = null;
+  const info = useField('text');
+  const infoCopy = { ...info };
+  info.reset = null;
 
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     navigate('/');
-    setNotification(`a new anecdote ${content} created!`);
+    setNotification(`a new anecdote ${content.value} created!`);
     setTimeout(() => {
       setNotification('');
     }, 3000);
@@ -29,30 +36,27 @@ const CreateNew = ({ addNew, setNotification }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
+      <button
+        onClick={() => {
+          contentCopy.reset();
+          authorCopy.reset();
+          infoCopy.reset();
+        }}
+      >
+        reset
+      </button>
     </div>
   );
 };
